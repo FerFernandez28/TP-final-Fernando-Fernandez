6377cc208e6663318102400a10ac84c40fb3b6a4
FerFernandez28 <ferchufm560@gmail.com>
2023-07-25 14:36:30 -0300

    MQ hechas

--- src/Components/ProductCard/ProductCard.jsx
+++ src/Components/ProductCard/ProductCard.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import './ProductCard.css'
 
 
 const ProductCard = ({producto}) => {
   return (
-    <Link to={'/detail/' + producto.id}>
+    <NavLink to={'/detail/' + producto.id}>
         <div className='productCardContainer'>
             <img src={producto.image} width={'100%'} height={'100%'} />
             <div className='containerInfo'>
@@ -14,7 +14,7 @@ const ProductCard = ({producto}) => {
               <h3 className='h3Precio'>$ {producto.precio}</h3>
             </div>
         </div>
-    </Link>
+    </NavLink>
   )
 }
 
